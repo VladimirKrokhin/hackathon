b1de159d39d407c473c3b7b9fdc56209453461a8
VlKrokhin <krokhinvladimir2004@gmail.com>
2025-11-15 18:12:13 +0500

    chore&fix: Удаляет неиспользуемые импорты

--- src/bot/handlers/__init__.py
+++ src/bot/handlers/__init__.py
@@ -1,7 +1,6 @@
 from aiogram import Router
 
 from .start import start_router
-from .questionnaire import questionnaire_router
 from .generation import generation_router
 from .new_generation import new_generation_router
 from .refactoring import refactoring_router
@@ -14,7 +13,6 @@ router = Router(name="main_router")
 router.include_router(start_router)
 router.include_router(ngo_info_router)
 router.include_router(new_generation_router)
-router.include_router(questionnaire_router)
 router.include_router(generation_router)
 router.include_router(refactoring_router)
 router.include_router(content_plan_router)
