a50cc5aa7cde7bfa18f661de7d1a431641018bc1
Vladimir Krokhin <119413075+VladimirKrokhin@users.noreply.github.com>
2025-11-15 18:16:49 +0500

    Генерация контент по запросу в свободной форме (#16)
    * docs: Архитектура приложения
    
    * feat: Генерация контента по свободному запросу
    
    * chore&fix: Удаляет неиспользуемые импорты

--- src/bot/handlers/__init__.py
+++ src/bot/handlers/__init__.py
@@ -1,8 +1,8 @@
 from aiogram import Router
 
 from .start import start_router
-from .questionnaire import questionnaire_router
 from .generation import generation_router
+from .new_generation import new_generation_router
 from .refactoring import refactoring_router
 from .content_plan import content_plan_router
 from .text_editing import text_editing_router
@@ -13,7 +13,7 @@ from .ngo_info import ngo_info_router
 router = Router(name="main_router")
 router.include_router(start_router)
 router.include_router(ngo_info_router)
-router.include_router(questionnaire_router)
+router.include_router(new_generation_router)
 router.include_router(generation_router)
 router.include_router(refactoring_router)
 router.include_router(content_plan_router)
